7e84a29dd42373e13898902103937ee6874d8392
LifeStyleUA <andriy6868@gmail.com>
2026-02-13 22:21:59 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# Токен бота (на Render задаём через Environment Variable)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Канал, на который нужно подписаться
+REQUIRED_CHANNEL = "open_lifestyle_ua"
+
+# Проверка подписки включена
+SUBSCRIPTION_CHECK_ENABLED = True
